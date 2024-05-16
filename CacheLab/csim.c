407999340689d5d4f079d3e3521e59ee1694d3d9
root <root@xQAQyn>
2024-05-16 10:42:44 +0800

    Cache Lab

--- CacheLab/csim.c
+++ CacheLab/csim.c
@@ -1,7 +1,145 @@
 #include "cachelab.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int main()
+typedef struct {
+    int valid;
+    int tag;
+    int last_used;
+} cache_line_t;
+
+typedef struct {
+    cache_line_t* lines;
+} cache_set_t;
+
+typedef struct {
+    cache_set_t* sets;
+} cache_t;
+
+#define VERBOSE_PRINTF(fmt, ...) do { if (verbose_mode) printf(fmt, __VA_ARGS__); } while (0)
+
+void parse_addr(unsigned addr, int* tag, int* set_index, int* block_offset, int set_index_bit, int block_bits){
+    *tag = addr >> (set_index_bit + block_bits);
+    *set_index = (addr >> block_bits) & ((1 << set_index_bit) - 1);
+    *block_offset = addr & ((1 << block_bits) - 1);
+}
+
+int find_match(cache_t* cache, int set_index, int tag, int lines){
+    for(int i = 0; i < lines; i++)
+        if(cache->sets[set_index].lines[i].valid && cache->sets[set_index].lines[i].tag == tag)
+            return i;
+    return -1;
+}
+
+int load_new_block(cache_t* cache, int set_index, int tag, int lines, int timestamp){
+    int line_index = -1, evic = 0;
+    for(int i = 0; i < lines; i++)
+        if(cache->sets[set_index].lines[i].valid == 0){
+            line_index = i;
+            break;
+        }
+    if(line_index == -1){
+        line_index = 0;
+        evic = 1;
+        for(int i = 1; i < lines; i++)
+            if(cache->sets[set_index].lines[i].last_used < cache->sets[set_index].lines[line_index].last_used)
+                line_index = i;
+    }
+    
+    cache->sets[set_index].lines[line_index].last_used = timestamp;
+    cache->sets[set_index].lines[line_index].tag = tag;
+    cache->sets[set_index].lines[line_index].valid = 1;
+
+    return evic;
+}
+
+int main(int argc, char *argv[])
 {
-    printSummary(0, 0, 0);
+    int hits = 0, misses = 0, evictions = 0;
+
+    int verbose_mode = 0;
+    int set_index_bit = -1, lines = -1, block_bits = -1;
+    FILE* tracefile = NULL;
+
+    for(int i = 1; i < argc; i++){
+        if(argv[i][0] == '-'){
+            switch(argv[i][1]){
+                case 'v':
+                    verbose_mode = 1;
+                    break;
+                case 's':
+                    set_index_bit = atoi(argv[++i]);
+                    break;
+                case 'E':
+                    lines = atoi(argv[++i]);
+                    break;
+                case 'b':
+                    block_bits = atoi(argv[++i]);
+                    break;
+                case 't':
+                    tracefile = fopen(argv[++i], "r");
+                    break;
+            }
+        }
+    }
+
+    cache_t cache;
+    cache.sets = (cache_set_t*)malloc(sizeof(cache_set_t) * (1 << set_index_bit));
+    for(int i = 0; i < (1 << set_index_bit); i++){
+        cache.sets[i].lines = (cache_line_t*)malloc(sizeof(cache_line_t) * lines);
+        for(int j = 0; j < lines; j++)
+            cache.sets[i].lines[j].valid = 0;
+    }
+    
+    char op[3];
+    int addr, size;
+    int timestamp = 0;
+    while(fscanf(tracefile, " %s %x,%d", op, &addr, &size) != EOF){
+        VERBOSE_PRINTF("%c %x,%d", op[0], addr, size);
+        int tag, set_index, block_offset, match;
+        parse_addr(addr, &tag, &set_index, &block_offset, set_index_bit, block_bits);
+        switch(op[0]){
+            case 'I': case 'L':
+                match = find_match(&cache, set_index, tag, lines);
+                if(match != -1){
+                    hits++;
+                    VERBOSE_PRINTF("%s"," hit");
+                    cache.sets[set_index].lines[match].last_used = timestamp;
+                }else{
+                    misses++;
+                    VERBOSE_PRINTF("%s"," miss");
+                    int evict = load_new_block(&cache, set_index, tag, lines, timestamp);
+                    if(evict){
+                        evictions++;
+                        VERBOSE_PRINTF("%s"," eviction");
+                    }
+                }
+                break;
+            case 'S': case 'M':
+                match = find_match(&cache, set_index, tag, lines);
+                if(match != -1){
+                    hits++;
+                    VERBOSE_PRINTF("%s"," hit");
+                    cache.sets[set_index].lines[match].last_used = timestamp;
+                }else{
+                    misses++;
+                    VERBOSE_PRINTF("%s"," miss");
+                    int evict = load_new_block(&cache, set_index, tag, lines, timestamp);
+                    if(evict){
+                        evictions++;
+                        VERBOSE_PRINTF("%s"," eviction");
+                    }
+                    VERBOSE_PRINTF("%s"," hit");
+                    hits++;
+                }
+                break;
+            
+        }
+        VERBOSE_PRINTF("%s","\n");
+        timestamp++;
+    }
+
+    printSummary(hits, misses, evictions);
     return 0;
 }
--- CacheLab/trans.c
+++ CacheLab/trans.c
@@ -11,6 +11,9 @@
 #include "cachelab.h"
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
+void transpose_32(int M, int N, int A[N][M], int B[M][N]);
+void transpose_64(int M, int N, int A[N][M], int B[M][N]);
+void transpose_61(int M, int N, int A[N][M], int B[M][N]);
 
 /* 
  * transpose_submit - This is the solution transpose function that you
@@ -20,15 +23,96 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
  *     be graded. 
  */
 char transpose_submit_desc[] = "Transpose submission";
-void transpose_submit(int M, int N, int A[N][M], int B[M][N])
-{
+void transpose_submit(int M, int N, int A[N][M], int B[M][N]){
+    if(N == 32) transpose_32(M, N, A, B);
+    else if(N == 64) transpose_64(M, N, A, B);
+    else transpose_61(M, N, A, B);
 }
 
 /* 
  * You can define additional transpose functions below. We've defined
  * a simple one below to help you get started. 
  */ 
+void transpose_32(int M, int N, int A[N][M], int B[M][N]){
+    int blocking = 8;
+    int a0, a1, a2, a3, a4, a5, a6, a7;
+    for(int i = 0; i < N; i += blocking)
+        for(int j = 0; j < M; j += blocking)
+            for(int k = i; k < i + blocking; k++){
+                a0 = A[k][j];
+                a1 = A[k][j + 1];
+                a2 = A[k][j + 2];
+                a3 = A[k][j + 3];
+                a4 = A[k][j + 4];
+                a5 = A[k][j + 5];
+                a6 = A[k][j + 6];
+                a7 = A[k][j + 7];
 
+                B[j][k] = a0;
+                B[j + 1][k] = a1;
+                B[j + 2][k] = a2;
+                B[j + 3][k] = a3;
+                B[j + 4][k] = a4;
+                B[j + 5][k] = a5;
+                B[j + 6][k] = a6;
+                B[j + 7][k] = a7;
+            }
+}
+
+void transpose_64(int M, int N, int A[N][M], int B[M][N]){
+    int blocking = 4;
+    int a0, a1, a2, a3;
+    for(int i = 0; i < N; i += blocking)
+        for(int j = 0; j < M; j += blocking)
+            if(i != j)
+                for(int k = i; k < i + blocking; k++){
+                    a0 = A[k][j];
+                    a1 = A[k][j + 1];
+                    a2 = A[k][j + 2];
+                    a3 = A[k][j + 3];
+                    B[j][k] = a0;
+                    B[j + 1][k] = a1;
+                    B[j + 2][k] = a2;
+                    B[j + 3][k] = a3;
+                }
+    for(int i = 0; i < N;i += blocking)
+        for(int j = i; j < i + blocking; j++){
+            a0 = A[j][i];
+            a1 = A[j][i + 1];
+            a2 = A[j][i + 2];
+            a3 = A[j][i + 3];
+            B[i][j] = a0;
+            B[i + 1][j] = a1;
+            B[i + 2][j] = a2;
+            B[i + 3][j] = a3;
+        }
+}
+
+void transpose_61(int M, int N, int A[N][M], int B[M][N]){
+    int blocking = 8;
+    int a0 = 0, a1 = 0, a2 = 0, a3 = 0, a4 = 0, a5 = 0, a6 = 0, a7 = 0;
+    for(int i = 0; i < N; i += blocking)
+        for(int j = 0; j < M; j += blocking)
+            for(int k = i; k < i + blocking && k < N; k++){
+                a0 = A[k][j];
+                if(j + 1 < M) a1 = A[k][j + 1];
+                if(j + 2 < M) a2 = A[k][j + 2];
+                if(j + 3 < M) a3 = A[k][j + 3];
+                if(j + 4 < M) a4 = A[k][j + 4];
+                if(j + 5 < M) a5 = A[k][j + 5];
+                if(j + 6 < M) a6 = A[k][j + 6];
+                if(j + 7 < M) a7 = A[k][j + 7];
+
+                B[j][k] = a0;
+                if(j + 1 < M) B[j + 1][k] = a1;
+                if(j + 2 < M) B[j + 2][k] = a2;
+                if(j + 3 < M) B[j + 3][k] = a3;
+                if(j + 4 < M) B[j + 4][k] = a4;
+                if(j + 5 < M) B[j + 5][k] = a5;
+                if(j + 6 < M) B[j + 6][k] = a6;
+                if(j + 7 < M) B[j + 7][k] = a7;
+            }
+}
 /* 
  * trans - A simple baseline transpose function, not optimized for the cache.
  */
@@ -42,7 +126,7 @@ void trans(int M, int N, int A[N][M], int B[M][N])
             tmp = A[i][j];
             B[j][i] = tmp;
         }
-    }    
+    }
 
 }
 
